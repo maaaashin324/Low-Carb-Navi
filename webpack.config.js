c9a0b5a9487d63d0fdb2b1a25f12abd6ec9a9c26
maaaashin324 <mstk.shin@gmail.com>
2018-02-12 17:23:21 +0900

    set initial configration of webpack

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const OpenBrowserPlugin = require('open-browser-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: [
+    'webpack/hot/only-dev-server',
+    `${path.resolve(__dirname, 'src')}/index.jsx`,
+  ],
+  module: {
+    loaders: [
+      {
+        loaders: 'style!css',
+        test: /\.css$/,
+      },
+      {
+        exclude: /(node_modules)/,
+        include: path.join(__dirname, 'src'),
+        loader: 'babel-loader',
+        test: /\.jsx$/,
+      },
+    ],
+  },
+  devTool: 'cheap-module-eval-source-map',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugin: [
+    new OpenBrowserPlugin({ url: 'http://localhost:8080' }),
+  ],
+  resolve: ['', '.webpack.js', 'js', 'jsx'],
+};
