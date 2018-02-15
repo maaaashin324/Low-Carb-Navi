f5ae2214daf7058042018b4fa4b1fa9947b9feeb
maaaashin324 <mstk.shin@gmail.com>
2018-02-15 16:15:15 +0900

    implement give the data to server in action in redux

--- src/actions/record.js
+++ src/actions/record.js
@@ -1,6 +1,29 @@
 module.exports = {
-  registRestaurant: dest => ({
-    type: 'REGISTERED',
-    dest,
-  }),
+  registRestaurant: (restaurantData) => {
+    const { name, address } = restaurantData;
+    const geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${address}`;
+    const thisAppUrl = 'http://127.0.0.1/restaurants';
+
+    fetch(geocodeUrl, { method: 'GET' })
+      .then(res => res.json())
+      .then((geoData) => {
+        const { location } = geoData.results[0].geometry;
+        return ({
+          name,
+          lng: location.lng,
+          lat: location.lat,
+        });
+      })
+      .then(locationData => fetch(thisAppUrl, {
+        body: JSON.stringify(locationData),
+        method: 'POST',
+      }))
+      .then(() => ({
+        type: 'REGISTERED',
+      }))
+      .catch(err => ({
+        type: 'ERROR',
+        err,
+      }));
+  },
 };
